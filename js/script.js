770a1646291c40185f7fec8e3993cf539452ea93
Tristan Harris <tris.dev@pumpcottage.me.uk>
2016-10-07 21:39:46 +0100

    Auto add rows

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 (function($) {
+  $('.route-card input').each(function() {
+    $(this).val($(this).attr('value'));
+  });
+
   var row = $('.route-card tbody tr').clone();
 
   function new_row() {
     $('.route-card tbody').append(row.clone());
   }
+
+  $('.route-card').on('keydown', 'tr:last-child .grid-ref input', function(e) {
+    new_row();
+  });
 
   new_row();
 
